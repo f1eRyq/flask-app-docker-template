be4685c50f279df6eeefdd86535200e17b0e7625
Andrey <351185@mail.ru>
2021-10-12 01:40:29 +0500

    name

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! I am not use Docker'
+    return 'Hello, World! I am use Docker'
 
-app.run()
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port=5000)
